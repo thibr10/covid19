44aba8ef1d2e66fff054f12b2f2349435a55ce65
thibr10 <tsbm10@yahoo.com.br>
2022-05-13 23:52:15 -0300

    atualizações

---
+++ build/precache-manifest.9152f7608fa731bdb18b488fcd7f98d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e89da47a56088354c42a",
+    "url": "/static/js/main.e89da47a.chunk.js"
+  },
+  {
+    "revision": "4506d79635d9a3615b5b",
+    "url": "/static/js/1.4506d796.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce09ee7114e1a82303167b3a544900ef",
+    "url": "/static/media/covid.ce09ee71.jpg"
+  },
+  {
+    "revision": "c98e1c22d40dcbd56d9d7f60b7a8a410",
+    "url": "/static/media/refresh.c98e1c22.svg"
+  },
+  {
+    "revision": "bc270c489b9c45d1714545c9e3747ecf",
+    "url": "/index.html"
+  }
+];
